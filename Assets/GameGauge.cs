7a4ea52bb131d3539bf66810299938a25d77aa3a
yunu.kim <kl1467@gmail.com>
2017-04-03 17:00:25 +0900

    IngameScene Outline

---
+++ Assets/GameGauge.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameGauge : MonoBehaviour
+{
+    Text text;
+    Slider slider;
+    void Start()
+    {
+        text = GetComponentInChildren<Text>();
+        slider = GetComponent<Slider>();
+    }
+
+
+    public void SetSliderValue(int value)
+    {
+        slider.value = value;
+        text.text = value.ToString();
+    }
+
+    public int GetSliderValue()
+    {
+        if (slider == null)
+            return 0;
+
+        return (int)slider.value;
+    }
+}
