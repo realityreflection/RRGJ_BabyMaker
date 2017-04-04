decb713c4b67912bf4d05fed3a1f678e4d7b0a9c
yunu.kim <kl1467@gmail.com>
2017-04-04 17:54:48 +0900

    GameController

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -7,12 +7,18 @@ public class GameController : MonoBehaviour {
 
     public GameGauge gauge;
     public Slider timerSlider;
+    public Text SkillText;
+    public Text FailedText;
+    public SexPanel SexPanel;
+    public CommandLayer MyCmdLayer;
+    public CommandLayer OpponentCmdLayer;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
-		
-	}
+        SkillText.enabled = false;
+        FailedText.enabled = false;
+    }
 	
 	// Update is called once per frame
 	void Update ()
